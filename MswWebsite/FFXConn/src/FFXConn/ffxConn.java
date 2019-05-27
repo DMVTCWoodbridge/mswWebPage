9c601f5105682a56a45f23a2457d5ed62069b7e8
DMVTCWoodbridge <50423946+DMVTCWoodbridge@users.noreply.github.com>
2019-05-26 20:09:15 -0700

    created a class named ffxConn in eclipse
    created a class named ffxConn in eclipse

---
+++ MswWebsite/FFXConn/src/FFXConn/ffxConn.java
@@ -0,0 +1,14 @@
+package FFXConn;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ffxConn {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\Selenium\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
