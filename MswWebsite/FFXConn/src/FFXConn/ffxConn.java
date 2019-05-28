d8ad12337a528b5023ae3e121a28b4e27ba4ccb5
DMVTCWoodbridge <50423946+DMVTCWoodbridge@users.noreply.github.com>
2019-05-28 14:14:11 -0700

    Changed url to dmvtrainingenter
    I changed URL to start working on DMVTC Website

--- MswWebsite/FFXConn/src/FFXConn/ffxConn.java
+++ MswWebsite/FFXConn/src/FFXConn/ffxConn.java
@@ -8,7 +8,7 @@ public class ffxConn {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\Documents\\Selenium\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
-		driver.get("https://www.msw4u.com");
+		driver.get("http://dmvtrainingcenter.com");
 	}
 
 }
